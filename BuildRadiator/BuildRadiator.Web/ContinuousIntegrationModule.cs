ce8c6c992e1347e0b1cbc0f534fb9bc5f6770200
Alastair Smith <alastair.smith@grantadesign.com>
2013-07-03 15:40:32 +0100

    Dump out all the TeamCity projects.
    Test to see whether these bits will work together. Connect to our TeamCity
    server, pull out all the projects, and dump them out on the page at
    /build/projects.
    
    (The TeamCity URL is redacted from this commit.)

---
+++ BuildRadiator/BuildRadiator.Web/ContinuousIntegrationModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using TeamCitySharp;
+using TeamCitySharp.DomainEntities;
+
+namespace BuildRadiator.Web
+{
+    public class ContinuousIntegrationModule : NancyModule
+    {
+        public ContinuousIntegrationModule() : base("/build")
+        {
+            Get["/projects"] = _ => string.Join("<br />", GetTeamCityProjects().Select(p => p.Name));
+        }
+
+        private IEnumerable<Project> GetTeamCityProjects()
+        {
+            var client = new TeamCityClient("");
+            client.Connect(string.Empty, string.Empty, actAsGuest: true);
+            return client.AllProjects();
+        }
+    }
+}
