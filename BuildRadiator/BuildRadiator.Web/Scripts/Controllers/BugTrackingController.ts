4e17f2831028a8e076663103991f4cce2eb1c93d
Alastair Smith <alastair.smith@grantadesign.com>
2013-07-07 18:04:54 +0100

    Render hard-coded data from BugTrackingController.
    This is just to get the necessary Angular pieces set up and working. The
    next step is to create a REST service in Nancy that the Angular code can
    call to get the necessary data.

--- BuildRadiator/BuildRadiator.Web/Scripts/Controllers/BugTrackingController.ts
+++ BuildRadiator/BuildRadiator.Web/Scripts/Controllers/BugTrackingController.ts
@@ -1,6 +1,8 @@
-function BugTrackingController($scope) {
-    $scope.projects = [
-        { "name": "Cross-Database Reporting and Charting", "openIssueCount": 15 },
-        { "name": "ToolboxWeb", "openIssueCount": 5 }
-    ];
+class BugTrackingController {
+    public BugTrackingController($scope) {
+        $scope.projects = [
+            { "name": "Cross-Database Reporting and Charting", "openIssueCount": 15 },
+            { "name": "ToolboxWeb", "openIssueCount": 5 }
+        ];
+    }
 }
