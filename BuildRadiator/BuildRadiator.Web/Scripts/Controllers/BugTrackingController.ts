b7fb43db23ba13e0b1fcc3753ab8dec97c22bd00
Alastair Smith <alastair.smith@grantadesign.com>
2013-07-07 14:10:47 +0100

    Add a Bug Tracking Controller.
    Testing out Angular - can't get it to work right now as Nancy is 404ing
    the JavaScript files...

---
+++ BuildRadiator/BuildRadiator.Web/Scripts/Controllers/BugTrackingController.ts
@@ -0,0 +1,6 @@
+function BugTrackingController($scope) {
+    $scope.projects = [
+        { "name": "Cross-Database Reporting and Charting", "openIssueCount": 15 },
+        { "name": "ToolboxWeb", "openIssueCount": 5 }
+    ];
+}
